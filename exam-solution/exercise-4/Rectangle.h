b5cc64ae33ae9b27ad526cd212d9bba9d6370910
toza-mimoza <55392933+toza-mimoza@users.noreply.github.com>
2019-11-01 21:59:23 +0100

    changed the name from setUpBox to initBox and removed the comment

--- exam-solution/exercise-4/Rectangle.h
+++ exam-solution/exercise-4/Rectangle.h
@@ -10,9 +10,8 @@ private:
 public:
 	Rectangle();
 	Rectangle(double h, double w);
-	//MOVE FOR RECT
 	void move(double dX, double dY);
-	void setUpBox();
+	void initBox();
 private:
 	void moveBox(double dX = 0, double dY = 0);
 	
